4c4aaef4d491345ca16fff27b8c26ff102498e30
Jorge Moura <jjorge.moura@gmail.com>
2023-08-26 15:35:51 +0100

    Adjust dependencies and address linting

--- Package.swift
+++ Package.swift
@@ -43,7 +43,7 @@ let package = Package(
     ],
 
     dependencies: [
-        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.0.0"),
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.1.0"),
         .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.0.0"),
         .package(url: "https://github.com/pointfreeco/swift-tagged", from: "0.10.0"),
         .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.0.0")
